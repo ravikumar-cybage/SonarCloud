3fc46100f04cea9b7bf8b0f665eaddb54aeaa544
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2026-01-08 00:52:42 +0530

    Update HomeControllerTests.cs

--- Tests/HomeControllerTests.cs
+++ Tests/HomeControllerTests.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+using Microsoft.Extensions.Logging.Abstractions;
 using WebApp.Controllers;
-
+ 
 namespace Tests;
-
+ 
 public class HomeControllerTests
 {
     [Fact]
     public void CreateController()
     {
         var sut = new HomeController(new NullLogger<HomeController>());
-
+ 
         Assert.NotNull(sut);
     }
 }
