062d28825985c130067a235d1761ba5e0154d86d
ravikumar-cybage <131234846+ravikumar-cybage@users.noreply.github.com>
2026-01-09 15:43:52 +0530

    Update HomeControllerTests.cs

--- Tests/HomeControllerTests.cs
+++ Tests/HomeControllerTests.cs
@@ -1,16 +1,42 @@
 using Xunit;
 using Microsoft.Extensions.Logging.Abstractions;
 using WebApp.Controllers;
- 
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
 namespace Tests;
- 
+
 public class HomeControllerTests
 {
-    [Fact]
-    public void CreateController()
+    private readonly HomeController _controller;
+
+    public HomeControllerTests()
     {
-        var sut = new HomeController(new NullLogger<HomeController>());
- 
-        Assert.NotNull(sut);
+        _controller = new HomeController(new NullLogger<HomeController>());
+    }
+
+    [Fact]
+    public void Index_ReturnsViewResult()
+    {
+        // Act
+        var result = _controller.Index() as ViewResult;
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void Privacy_ReturnsViewResult()
+    {
+        var result = _controller.Privacy() as ViewResult;
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void Error_ReturnsViewWithErrorModel()
+    {
+        var result = _controller.Error() as ViewResult;
+        Assert.NotNull(result);
+        Assert.IsType<ErrorViewModel>(result.Model);
     }
 }
